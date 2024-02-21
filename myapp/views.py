37ffc805bceff2270bc533f2dea4dc08b6e2e9d0
dhimanhere <dhimanhere96@gmail.com>
2024-02-22 04:38:25 +0530

    added registration and login form

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
 	return render(request, "myapp/home.html")
+
+def filterbooks(request):
+	pass
+
+def registerview(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = request.POST['username']
+			password = request.POST['password2']
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return redirect("/")
+		else:
+			messages.error(request, "Something went wrong!")
+	else:
+		form = UserCreationForm()
+	context = {
+		'form':form,
+	}
+	return render(request, 'myapp/register.html', context)
+
+def loginview(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			next_url = request.POST.get("next", "")
+			if next_url:
+				return redirect(next_url)
+			else:
+				return redirect("/")
+		else:
+			messages.error(request, "Something went wrong")
+	else:
+		messages.warning(request, "This is our fault")
+	return render(request, 'myapp/login.html')
